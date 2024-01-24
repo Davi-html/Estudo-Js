531bfbebaf9f177406f47eabe93d2cfbb44bccfe
Davi Fernandes Alves <126309063+Davi-html@users.noreply.github.com>
2024-01-24 05:08:06 +0000

    atualizando

---
+++ Array(avançado)/01 revisao.js
@@ -0,0 +1,44 @@
+// const nome1 = ['Carlos', 'Eduardo', 'Joana']
+
+// delete nome1[1]
+
+// console.log(nome1)
+
+///////////////////////////////////////////////////////////////
+
+// const nome2 = new Array('Davi', 'Lucas', 'Joao')
+
+// console.log(nome2)
+
+/////////////////////////////////////////////////////////////////
+
+// const nome3 = ['Davi', 'Lucas', 'Joao']
+// const novo = [...nome3]
+
+// novo.pop()
+
+// console.log(novo)
+// console.log(nome3)
+
+////////////////////////////////////////////////////////////////////
+
+// const nome3 = ['Davi', 'Lucas', 'Joao']
+// novo = nome3.slice(0, -2)
+
+// console.log(novo)
+
+//////////////////////////////////////////////////////////////////////
+
+// const nome = 'Davi Fernandes Alves'
+
+// const array = nome.split(' ')
+
+// console.log(array)
+
+//////////////////////////////////////////////////////////////////////
+
+const nomes = ['Davi', 'Fernandes', 'Lucas']
+
+const string = nomes.join(' ')
+
+console.log(string)
---
+++ Array(avançado)/02 splice.js
@@ -0,0 +1,20 @@
+const nomes = ['Davi', 'Lucas', 'Joao', 'Julia', 'Gabriel', 'Eduardo']
+
+// nomes.splice(índice, delete, adicionar1, adicionar2, adicionar3...)
+
+// pop
+// const removidos = nomes.splice(-1, 1)
+
+// push
+// const removidos = nomes.splice(nomes.length, 0, 'Adriano')
+
+// shift
+// const removidos = nomes.splice(0, 1)
+
+// unshift
+// const removidos = nomes.splice(0, 0, 'Adriano')
+
+
+
+console.log(nomes)
+console.log(removidos)
